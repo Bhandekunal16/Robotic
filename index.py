05990ecc3b0c154365f31aeb92f035540ba7733c
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-09 17:01:55 +0530

    Deleted .gitignore, brain.py, lab.py, labLocation.py, labString.py, store.py and index.bin files. Also, removed cached files in __pycache__/store.cpython-3

---
+++ index.py
@@ -0,0 +1,56 @@
+from service import service
+import os
+
+
+def application():
+        current_file_path = os.path.abspath(__file__)
+        task : str = input(f'{current_file_path}$:')
+        if task == 'write':
+            newTask : str = input('enter file name :')
+            service.write(newTask)
+        elif task == 'read':
+            newTask : str = input('enter file name :')
+            service.read(newTask)
+        elif task == 'readline':
+            newTask : str = input('enter file name :')
+            service.read(newTask)
+        elif task == 'edit':
+            newTask : str = input('enter file name :')
+            content : str = input('enter content :')
+            service.edit(newTask, content)
+        elif task == 'mkdir':
+            newTask : str = input('enter folder name :')
+            service.mkdir(newTask)
+        elif task == 'remove':
+            newTask : str = input('enter folder name :')
+            service.remove(newTask)
+        elif task == 'rmdir':
+            newTask : str = input('enter folder name :')
+            service.rmdir(newTask)
+        elif task == 'cd':
+            newTask : str = input('enter folder name :')
+            service.cd(newTask)
+        elif task == 'simulate':
+            service.long_running_task()
+        elif task == 'memory':
+            service.memory()
+        elif task == 'cpu':
+            service.cpu()
+        elif task == 'exit':
+            'exit'
+        elif task == 'copy':
+            newTask : str = input('enter file name :')
+            newTask2 : str = input('enter destination for the file :')
+            service.copy(newTask, newTask2 )
+        elif task == 'rename':
+            newTask : str = input('enter old file name :')
+            newTask2 : str = input('enter new file name :')
+            service.copy(newTask, newTask2 )
+        elif task == 'chmod':
+            newTask : str = input('enter file name :')
+            service.chmod(newTask)
+        elif task == 'li':
+            newTask : str = input('enter file name :')
+            service.list(newTask)
+
+application()
---
+++ service.py
@@ -0,0 +1,78 @@
+import os
+import time
+import psutil
+import shutil
+
+class service :
+    def write(name):
+        with open(name, 'w') as file:
+            file.write('//Hello, world!\n')
+            file.write('//This is a new file created using Python.')
+
+    def read(name):
+        with open(name, 'r') as file:
+            contents = file.read()
+            print(contents)
+
+    def lineRead(name):
+        with open(name, 'r') as file:
+            for line in file:
+                print(line.strip())
+            
+    def edit(name, Content):
+        with open(name, 'a') as file:
+            file.write(Content)
+            
+    def mkdir(name):
+        os.makedirs(name, exist_ok=True)
+        if os.path.exists(name):
+            print("Folder created successfully.")
+        else:
+            print("Failed to create folder.")
+            
+    def remove(name):
+        try:
+            os.remove(name)
+            print("File removed successfully.")
+        except OSError as e:
+            print(f"Error: {name} : {e.strerror}")
+            
+    def rmdir(name):
+        try:
+            os.rmdir(name)
+            print("Folder removed successfully.")
+        except OSError as e:
+            print(f"Error: {name} : {e.strerror}")
+            
+    def cd(name):
+        try:
+            os.chdir(name)
+            print("Current working directory changed to:", os.getcwd())
+        except OSError as e:
+            print(f"Error: {e.strerror}")
+            
+    def long_running_task():
+        for i in range(10):
+            time.sleep(1)  
+            print(f"Progress: {i+1}/10")
+            
+    def memory():
+        mem = psutil.virtual_memory()
+        print(f"Total Memory: {mem.total}")
+        print(f"Available Memory: {mem.available}")
+        
+    def cpu():
+        print(f"CPU Usage: {psutil.cpu_percent()}%")
+        
+    def copy(source, destination):
+        shutil.copy(source, destination)
+
+    def rename(old_name, new_name):
+        os.rename(old_name, new_name)
+        
+    def chmod(name):
+        os.chmod(name, 0o755)
+        
+    def list(name):
+        contents = os.listdir(name)
+        print(contents)
