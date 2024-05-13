d5337651cdf90effffbde3dd7ba0589326016b1e
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-13 16:14:30 +0530

    "Removed print statement comparing check and hashed\_string; added if-else statement to check for key match"

--- algorithm.py
+++ algorithm.py
@@ -25,9 +25,7 @@ class algorithm:
             nextNewString = newString.replace(nextModified_string, "")
 
             check = newString.replace(nextNewString, "")
-
-            print(check == hashed_string)
-
+        
             if check != hashed_string:
                 print("Key not matched")
             else:
