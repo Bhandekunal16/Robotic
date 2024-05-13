b7f080b9d8b1b506dee6b806e6e2232bfb0dcd47
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-13 17:51:13 +0530

    Algorithm class: updated variable declarations with type annotations
    * `tail` and `head` variables in `decrypt` method now have type annotations (str)
    * No changes in functionality, only formatting and readability

--- algorithm.py
+++ algorithm.py
@@ -13,18 +13,18 @@ class algorithm:
 
     def decrypt(publicKey, privateKey, data):
         try:
-            tail : str = service.hex(publicKey)
-            head : str = service.hex(privateKey)
+            tail: str = service.hex(publicKey)
+            head: str = service.hex(privateKey)
             hashed_string = hashlib.sha256(tail.encode()).hexdigest()
             data
 
-            modified_string : str = re.escape(head)
-            nextModified_string : str = re.escape(hashed_string)
+            modified_string: str = re.escape(head)
+            nextModified_string: str = re.escape(hashed_string)
 
-            newString : str = data.replace(modified_string, "")
-            nextNewString : str = newString.replace(nextModified_string, "")
+            newString: str = data.replace(modified_string, "")
+            nextNewString: str = newString.replace(nextModified_string, "")
 
-            check : str = newString.replace(nextNewString, "")
+            check: str = newString.replace(nextNewString, "")
 
             if check != hashed_string:
                 print("Key not matched")
