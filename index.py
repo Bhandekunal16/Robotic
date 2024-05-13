fcb41b8bd496146a83f2e33a10a41bcb67c393a8
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-13 10:31:35 +0530

    Change in index.py: function application is converted to a method in a new module class; no functional changes.
    The function application in index.py has been converted into a method in a new class named module. However, there are no

--- index.py
+++ index.py
@@ -2,96 +2,99 @@ from service import service
 from color import Color
 import os
 
+class module :
+    def application(task):
+        """
+        application that sort the os function according your choice and do your work
+        """
+        match task :
+            case 'write':
+                newTask : str = input('enter file name :')
+                service.write(newTask)
+            case 'read' :
+                newTask : str = input('enter file name :')
+                service.read(newTask)
+            case 'readline':
+                newTask : str = input('enter file name :')
+                service.lineRead(newTask)
+            case  'edit':
+                newTask : str = input('enter file name :')
+                content : str = input('enter content :')
+                service.edit(newTask, content)
+            case  'mkdir':
+                newTask : str = input('enter folder name :')
+                service.mkdir(newTask)
+            case  'remove':
+                newTask : str = input('enter folder name :')
+                service.remove(newTask)
+            case  'rmdir':
+                newTask : str = input('enter folder name :')
+                service.rmdir(newTask)
+            case  'simulate':
+                service.long_running_task()
+            case  'memory':
+                service.memory()
+            case  'cpu':
+                service.cpu()
+            case  'exit':
+                return 'exit'
+            case  'copy':
+                newTask : str = input('enter file name :')
+                newTask2 : str = input('enter destination for the file :')
+                service.copy(newTask, newTask2 )
+            case  'rename':
+                newTask : str = input('enter old file name :')
+                newTask2 : str = input('enter new file name :')
+                service.copy(newTask, newTask2 )
+            case  'chmod':
+                newTask : str = input('enter file name :')
+                service.chmod(newTask)
+            case  'ls':
+                current_dir = current_dir = os.getcwd()
+                service.list(current_dir)
+            case  'temperature':
+                service.temperature()
+            case  'boot':
+                service.bootTime()
+            case  'unknown':
+                service.getloadavg()
+            case  'net':
+                service.net_connections()
+            case  'address':
+                service.address()
+            case  'stats':
+                service.stats()
+            case  'counter':
+                service.counters()
+            case  'pid':
+                service.pids()
+            case  'battery':
+                service.battery()
+            case  'fans':
+                service.fans()
+            case  'users':
+                service.user()
+            case 'clear':
+                service.clear()
+            case 'node c':
+                newTask : str = input('enter file name :')
+                service.node(newTask)
+            case 'permission':
+                service.permission()
+            case 'python --v':
+                service.version()
+            case 'task':
+                newTask : str = input('enter the task :')
+                service.versions(newTask)
+                
+            case 'shutdown':
+                service.shutdown()
+                
+            case 'restart':
+                service.restart()
+                
+            
 
-def application(task):
-    """
-    application that sort the os function according your choice and do your work
-    """
-    match task :
-        case 'write':
-            newTask : str = input('enter file name :')
-            service.write(newTask)
-        case 'read' :
-            newTask : str = input('enter file name :')
-            service.read(newTask)
-        case 'readline':
-            newTask : str = input('enter file name :')
-            service.lineRead(newTask)
-        case  'edit':
-            newTask : str = input('enter file name :')
-            content : str = input('enter content :')
-            service.edit(newTask, content)
-        case  'mkdir':
-            newTask : str = input('enter folder name :')
-            service.mkdir(newTask)
-        case  'remove':
-            newTask : str = input('enter folder name :')
-            service.remove(newTask)
-        case  'rmdir':
-            newTask : str = input('enter folder name :')
-            service.rmdir(newTask)
-        case  'simulate':
-            service.long_running_task()
-        case  'memory':
-            service.memory()
-        case  'cpu':
-            service.cpu()
-        case  'exit':
-            return 'exit'
-        case  'copy':
-            newTask : str = input('enter file name :')
-            newTask2 : str = input('enter destination for the file :')
-            service.copy(newTask, newTask2 )
-        case  'rename':
-            newTask : str = input('enter old file name :')
-            newTask2 : str = input('enter new file name :')
-            service.copy(newTask, newTask2 )
-        case  'chmod':
-            newTask : str = input('enter file name :')
-            service.chmod(newTask)
-        case  'ls':
-            current_dir = current_dir = os.getcwd()
-            service.list(current_dir)
-        case  'temperature':
-            service.temperature()
-        case  'boot':
-            service.bootTime()
-        case  'unknown':
-            service.getloadavg()
-        case  'net':
-            service.net_connections()
-        case  'address':
-            service.address()
-        case  'stats':
-            service.stats()
-        case  'counter':
-            service.counters()
-        case  'pid':
-            service.pids()
-        case  'battery':
-            service.battery()
-        case  'fans':
-            service.fans()
-        case  'users':
-            service.user()
-        case 'clear':
-            service.clear()
-        case 'node c':
-            newTask : str = input('enter file name :')
-            service.node(newTask)
-        case 'permission':
-            service.permission()
-        case 'python --v':
-            service.version()
-        case 'task':
-            newTask : str = input('enter the task :')
-            service.versions(newTask)
-            
-        case 'shutdown':
-            service.shutdown()
-            
-        case 'restart':
-            service.restart()
 
 def tasks():
     try :
@@ -102,9 +105,9 @@ def tasks():
             task = input(Color.YELLOW + f'Welcome to the mystical castle of{Color.CYAN } {os.getcwd()}.{Color.YELLOW }What would you like to explore today?:\n' + Color.RESET)
             if 'cd' in task:
                 service.cd(task)
-                result = application(task)
+                result = module.application(task)
             else:
-                result = application(task)
+                result = module.application(task)
                 if result == 'exit':
                     print(Color.MAGENTA + '********** thank you! ***********')
                     break
