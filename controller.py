78f87668df4f4ede07997704ed9cd31a325b188f
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-14 16:55:58 +0530

    Update controller.py: add route for root endpoint, add whitespace, and update debug mode configuration
    The changes include:
    
    * Adding a new route for the root endpoint that returns "hello world"
    * Adding whitespace

--- controller.py
+++ controller.py
@@ -1,38 +1,42 @@
 from flask import Flask, request, jsonify
 from algorithm import algorithm
 from route import Route
-from environment import environment 
+from environment import environment
 from response import Response
 
 app = Flask(__name__)
-    
-@app.route('/', methods=['GET'])
-def get_books():
-    return 'hello world'
 
-@app.route(Route.encrypt, methods=['POST'])
+
+@app.route("/", methods=["GET"])
+def get_books():
+    return "hello world"
+
+
+@app.route(Route.encrypt, methods=["POST"])
 def encrypt():
     privateKey = environment.privateKey
     body = request.get_json()
-    if 'publicKey' in body and 'data' in body:
-        public_key = body['publicKey']
-        data = body['data']
+    if "publicKey" in body and "data" in body:
+        public_key = body["publicKey"]
+        data = body["data"]
         response = algorithm.encrypt(public_key, privateKey, data)
         return Response.encryption(response)
 
-@app.route(Route.decrypt, methods=['POST'])
+
+@app.route(Route.decrypt, methods=["POST"])
 def decrypt():
     privateKey = environment.privateKey
     body = request.get_json()
-    if 'publicKey' in body and 'data' in body:
-        public_key = body['publicKey']
-        data = body['data']
+    if "publicKey" in body and "data" in body:
+        public_key = body["publicKey"]
+        data = body["data"]
         response = algorithm.decrypt(public_key, privateKey, data)
         print(response)
-        if response == None :
-            return Response.notfound(f'{public_key} is matched with your public key')
-        else :
+        if response == None:
+            return Response.notfound(f"{public_key} is matched with your public key")
+        else:
             return Response.decryption(response)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
