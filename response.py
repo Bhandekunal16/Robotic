437ce51b1e2052d941ab8b547c340a69c1ab4f56
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-14 16:57:11 +0530

    Response class refactored to return status code, message, and data in a dictionary; JSONify response and status code returned separately.
    Specific changes:
    
    * Encryption method refactored to return a tuple containing the JSONified

--- response.py
+++ response.py
@@ -1,11 +1,32 @@
 from flask import jsonify
 
+
 class Response:
     def encryption(data):
-        return jsonify({'encrypted': data, 'status': True, 'statusCode': 200, 'msg': 'success'}), 200
-    
+        return (
+            jsonify(
+                {"encrypted": data, "status": True, "statusCode": 200, "msg": "success"}
+            ),
+            200,
+        )
+
     def decryption(data):
-        return jsonify({'decrypted': data, 'status': True, 'statusCode': 200, 'msg': 'success'}), 200
-    
+        return (
+            jsonify(
+                {"decrypted": data, "status": True, "statusCode": 200, "msg": "success"}
+            ),
+            200,
+        )
+
     def notfound(data):
-        return jsonify({'encrypted': data, 'status': False, 'statusCode': 404, 'msg': 'not found'}), 404
+        return (
+            jsonify(
+                {
+                    "encrypted": data,
+                    "status": False,
+                    "statusCode": 404,
+                    "msg": "not found",
+                }
+            ),
+            404,
+        )
