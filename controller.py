afd393e2b2a52e1a9ed562a8f27caf1ab62cd346
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-15 10:12:38 +0530

    "Use environment variable for privateKey in encrypt and decrypt functions"
    The diff applies changes to the file controller.py. It modifies the encrypt and decrypt functions by replacing the local variable privateKey with the environment variable environment

--- controller.py
+++ controller.py
@@ -14,23 +14,21 @@ def get_books():
 
 @app.route(Route.encrypt, methods=["POST"])
 def encrypt():
-    privateKey = environment.privateKey
     body = request.get_json()
     if "publicKey" in body and "data" in body:
         public_key = body["publicKey"]
         data = body["data"]
-        response = algorithm.encrypt(public_key, privateKey, data)
+        response = algorithm.encrypt(public_key, environment.privateKey, data)
         return Response.encryption(response)
 
 
 @app.route(Route.decrypt, methods=["POST"])
 def decrypt():
-    privateKey = environment.privateKey
     body = request.get_json()
     if "publicKey" in body and "data" in body:
         public_key = body["publicKey"]
         data = body["data"]
-        response = algorithm.decrypt(public_key, privateKey, data)
+        response = algorithm.decrypt(public_key, environment.privateKey, data)
         print(response)
         if response == None:
             return Response.notfound(f"{public_key} is matched with your public key")
