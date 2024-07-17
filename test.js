2cb562be058f3bbd7f5f0c7f29557ac39f18f6c8
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-17 15:25:14 +0530

    "Updated robotic.file.converter package to version 1.1.1, removed robotic-creater package, and refactored test.js and core.js files."

--- test.js
+++ test.js
@@ -1,35 +1,14 @@
-const Response = 
-    require("robotic.js/src/class/response");
 
-class  Binary {  convertToBinary(input) {    
-    try  {      
-        if  (input == undefined)         return   new Response().badRequest("input not be undefined");
-      
-        if  (typeof input !== "string")        throw new Response().badRequest("Input must be a string");
-
-        let binary = "";
-      for (let i = 0; i < input.length; i++) {
-        let charCode = input.charCodeAt(i).toString(2);
-        binary += this.padLeft(charCode, 8);
-}       return   binary;
+class  Core {    patterns = {      a: [" *** ", "*   *", "*****", "*   *", "*   *"],      b: ["**** ", "*   *", "**** ", "*   *", "**** "],      c: [" *** ", "*   *", "*    ", "*   *", " *** "],      d: ["**** ", "*   *", "*   *", "*   *", "**** "],      e: ["*****", "*    ", "**** ", "*    ", "*****"],      f: ["*****", "*    ", "**** ", "*    ", "*    "],      g: [" *** ", "*    ", "* ***", "*   *", " *** "],      h: ["*   *", "*   *", "*****", "*   *", "*   *"],      i: ["*****", "  *  ", "  *  ", "  *  ", "*****"],      j: ["  ***", "   * ", "   * ", "*  * ", " **  "],      k: ["*   *", "*  * ", "***  ", "*  * ", "*   *"],      l: ["*    ", "*    ", "*    ", "*    ", "*****"],      m: ["*   *", "** **", "* * *", "*   *", "*   *"],      n: ["*   *", "**  *", "* * *", "*  **", "*   *"],      o: [" *** ", "*   *", "*   *", "*   *", " *** "],      p: ["**** ", "*   *", "**** ", "*    ", "*    "],      q: [" *** ", "*   *", "*   *", "*  **", " ****"],      r: ["**** ", "*   *", "**** ", "*  * ", "*   *"],      s: [" ****", "*    ", " *** ", "    *", "**** "],      t: ["*****", "  *  ", "  *  ", "  *  ", "  *  "],      u: ["*   *", "*   *", "*   *", "*   *", " *** "],      v: ["*   *", "*   *", "*   *", " * * ", "  *  "],      w: ["*   *", "*   *", "* * *", "* * *", " * * "],      x: ["*   *", " * * ", "  *  ", " * * ", "*   *"],      y: ["*   *", "*   *", " ****", "    *", " *** "],      z: ["*****", "   * ", "  *  ", " *   ", "*****"],};
+      application(inputWord) {      inputWord = inputWord.toLowerCase();
+        const patternHeight = 5;
+      for (let row = 0; row < patternHeight; row++) {
+        let line = "";
+        for (let char of inputWord) {          
+        if  (this.patterns[char]) {            line += this.patterns[char][row] + "  ";
 } 
-    catch  (error) {       return   new Response().error(error);
-}}  convertToString = (input) => {    
-    try  {
-        let string = "";
-      
-        if  (input == undefined)         return   new Response().badRequest("input not be undefined");
-      
-        if  (typeof input !== "string")        throw new Response().badRequest("Input must be a string");
-      for (let i = 0; i < input.length; i += 8) {
-        let binaryCode = input.substr(i, 8);
-
-        let charCode = parseInt(binaryCode, 2);
-        string += String.fromCharCode(charCode);
-}       return   string;
-} 
-    catch  (error) {       return   error;
-}};
-  padLeft(str, length) {     return   "0".repeat(length - str.length) + str;
-}}
-module .exports = Binary;
+        else  {            line += "     ";
+}}        console.log(line);
+}}}    
+module .exports = Core;
+  
