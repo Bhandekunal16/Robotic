fa6f4c0d39d34e30b963c177a9dfcd3ebf831ec0
bhandekunal16 <bhandekunal16@gmail.com>
2024-07-17 13:57:07 +0530

    "Refactor formator.js, remove robotic-creater dependency, and update package-lock.json and package.json accordingly"

--- formator.js
+++ formator.js
@@ -1,12 +1,14 @@
-const shape = 
-    require("robotic.file.converter/converter.v2");
+const shape = require("robotic.file.converter/converter.v2");
 
-class  App {  application(path) {    new shape().readLines(path);
-}}const app = new App();
+class App {
+  application(path) {
+    new shape().readLines(path);
+  }
+}
+const app = new App();
 
-        if  (
-    require.main === 
-module ) {  const path = process.argv[2];
+if (require.main === module) {
+  const path = process.argv[2];
   app.application(path);
 }
-module .exports = App;
+module.exports = App;
